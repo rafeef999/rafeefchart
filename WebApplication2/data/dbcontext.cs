aaaf7a302eb4b6aee063efcab4d55c64708ad7ea
rafeef99 <samedk2000@gmail.com>
2023-02-16 14:09:16 +0200

    rcommit

--- WebApplication2/data/dbcontext.cs
+++ WebApplication2/data/dbcontext.cs
@@ -3,12 +3,12 @@ using Microsoft.EntityFrameworkCore;
 using WebApplication2.Models;
 namespace WebApplication2.data
 {
-    public class dbcontext
+    public class dbcontext:DbContext
     {
         public dbcontext(DbContextOptions<dbcontext> options) : base(options)
         {
 
         }
-        public DbSet<catagory> Catagories { get; set; }
+        public DbSet<chartr> Catagories { get; set; }
     }
 }
